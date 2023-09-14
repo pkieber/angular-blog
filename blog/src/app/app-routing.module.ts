4c1cdcf31893dbcead018acf6de25da601287fa3
pkieber <113725311+pkieber@users.noreply.github.com>
2023-09-14 11:25:08 +0200

    add components + routing

--- blog/src/app/app-routing.module.ts
+++ blog/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BlogComponent } from './components/blog/blog.component';
+import { SinglePostComponent } from './components/single-post/single-post.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'blogs', component: BlogComponent},
+  {path: 'blogs/:id', component: SinglePostComponent}
+  {path: '**', redirectTo: 'blogs', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- blog/src/app/app.module.ts
+++ blog/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { SinglePostComponent } from './components/single-post/single-post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogComponent,
+    SinglePostComponent
   ],
   imports: [
     BrowserModule,
